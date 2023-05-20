eb631c10ce0e27821240c538ff6058743db8a1e9
mcesari01 <105780721+mcesari01@users.noreply.github.com>
2023-05-20 15:11:51 +0200

    Update Talk.js

--- Lambda_Functions/Get_Watch_Next/Talk.js
+++ Lambda_Functions/Get_Watch_Next/Talk.js
@@ -1,1 +1,13 @@
+const mongoose = require('mongoose');
 
+const talk_schema = new mongoose.Schema({
+    _id: String,
+    title: String,
+    url: String,
+    details: String,
+    main_author: String,
+    num_views: String,
+    watch_next_s:  [String]
+}, { collection: 'tedx_data' });
+
+module.exports = mongoose.model('talk', talk_schema);
